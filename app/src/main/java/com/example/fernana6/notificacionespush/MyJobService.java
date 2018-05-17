59fc98eee733671f5abd4a3dbf8e21245063d1c2
EU\FernanA6 <alberto.fernandezseral@unisys.com>
2018-05-17 17:03:28 +0200

    Final

---
+++ app/src/main/java/com/example/fernana6/notificacionespush/MyJobService.java
@@ -0,0 +1,8 @@
+package com.example.fernana6.notificacionespush;
+
+/**
+ * Created by FernanA6 on 16-May-18.
+ */
+
+public class MyJobService {
+}
---
+++ app/src/main/java/com/example/fernana6/notificacionespush/NotificationMessage.java
@@ -0,0 +1,8 @@
+package com.example.fernana6.notificacionespush;
+
+/**
+ * Created by FernanA6 on 17-May-18.
+ */
+
+public class NotificationMessage {
+}
